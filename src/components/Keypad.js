f3c90b210b2a0e0efb0eab85eb7822afa9630999
G483 <gabrijel.web@gmail.com>
2018-06-20 10:33:32 +0200

    Updates

--- src/components/Keypad.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import '../css/Keypad.css';
-
-const Keypad = (props) => {
-    return <input type="submit" className={"key " + (props.extraClasses || '')} onClick={props.action} value={props.symbol} />
-}
-
-export default Keypad;
